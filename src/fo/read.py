6ddaa0c14eab937230f6f1d05f22ba63811e8550
towienko <01169218@pw.edu.pl>
2025-01-13 18:17:00 +0100

    Init repo

---
+++ src/fo/read.py
@@ -0,0 +1,14 @@
+import numpy as np
+import optuna
+
+fdatabase  = 'sqlite:///IllustrisTNG_o3_T_all_steps_500_500_o3.db'
+study_name = 'wd_dr_hidden_lr_o3'
+
+study = optuna.load_study(study_name=study_name, storage=fdatabase)
+
+trial = study.best_trial
+print("Best trial:  number {}".format(trial.number))
+print("Loss:        %.5e"%trial.value)
+print("Params: ")
+for key, value in trial.params.items():
+    print("    {}: {}".format(key, value))
